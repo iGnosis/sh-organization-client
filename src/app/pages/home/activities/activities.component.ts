6c3f6f8d9e96cdf62ccb65e881a06c9db15f9af7
Aman Gautam <aman@pointmotioncontrol.com>
2021-12-28 14:52:30 +0530

    implemented activities view

--- src/app/pages/home/activities/activities.component.ts
+++ src/app/pages/home/activities/activities.component.ts
@@ -7,6 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivitiesComponent implements OnInit {
 
+  activities = [
+    {
+      name: 'Warm up',
+      img: 'https://picsum.photos/id/1025/300/300',
+    },
+    {
+      name: 'Arm Stretch',
+      img: 'https://picsum.photos/id/1025/300/300',
+    },
+    {
+      name: 'Leg Stretch',
+      img: 'https://picsum.photos/id/1025/300/300',
+    },
+    {
+      name: 'Hallelujah',
+      img: 'https://picsum.photos/id/1025/300/300',
+    },
+    {
+      name: 'Plank',
+      img: 'https://picsum.photos/id/1025/300/300',
+    },
+    {
+      name: 'Jumping',
+      img: 'https://picsum.photos/id/1025/300/300',
+    },
+    {
+      name: 'Backflip',
+      img: 'https://picsum.photos/id/1025/300/300',
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
