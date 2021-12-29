07ceb817c879b63febf383221f0265d768ca127a
Aman Gautam <aman@pointmotioncontrol.com>
2021-12-29 15:27:00 +0530

    fixing the build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  cognitoUserPoolID: 'us-east-1_INWkmpaSb',
+  cognitoClientID: '4pc04vcpfodcgac8nk5svm911i',
+  gqlEndpoint: 'https://api.dev.pointmotioncontrol.com/v1/graphql'
 };
