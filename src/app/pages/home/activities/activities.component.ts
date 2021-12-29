cdccf04ab8097a1891de521644310d1434457683
Aman Gautam <aman@pointmotioncontrol.com>
2021-12-29 12:38:06 +0530

    activities updated

--- src/app/pages/home/activities/activities.component.ts
+++ src/app/pages/home/activities/activities.component.ts
@@ -11,30 +11,37 @@ export class ActivitiesComponent implements OnInit {
     {
       name: 'Warm up',
       img: 'https://picsum.photos/id/1025/300/300',
+      time: '3:00'
     },
     {
       name: 'Arm Stretch',
       img: 'https://picsum.photos/id/1025/300/300',
+      time: '2:30'
     },
     {
       name: 'Leg Stretch',
       img: 'https://picsum.photos/id/1025/300/300',
+      time: '3:15'
     },
     {
       name: 'Hallelujah',
       img: 'https://picsum.photos/id/1025/300/300',
+      time: '5:00'
     },
     {
       name: 'Plank',
       img: 'https://picsum.photos/id/1025/300/300',
+      time: '2:00'
     },
     {
       name: 'Jumping',
       img: 'https://picsum.photos/id/1025/300/300',
+      time: '3:00'
     },
     {
       name: 'Backflip',
       img: 'https://picsum.photos/id/1025/300/300',
+      time: '10:00'
     },
   ]
 
