08b8a4fa7aa6b25a01cacc772d40553fd8e9c765
Aman Gautam <aman@pointmotioncontrol.com>
2022-02-06 16:50:23 +0530

    auth related services

---
+++ src/app/services/auth/auth.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  baseURL: string = ''
+  constructor(private http: HttpClient) {
+    this.baseURL = environment.servicesEndpoint
+  }
+
+  login(details: any) {
+    return this.http.post(this.baseURL+'/auth/login', details)
+  }
+
+
+}
