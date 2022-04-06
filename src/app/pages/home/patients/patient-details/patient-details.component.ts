922db2758c510a22570c7815342858561f684fa6
Deep Bhatt <dbhatt@pointmotion.us>
2022-04-06 14:24:06 +0530

    add feature to redirect a user to activity

--- src/app/pages/home/patients/patient-details/patient-details.component.ts
+++ src/app/pages/home/patients/patient-details/patient-details.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 import { GqlConstants } from 'src/app/services/gql-constants/gql-constants.constants';
 import { GraphqlService } from 'src/app/services/graphql/graphql.service';
 import { Patient } from 'src/app/types/patient';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-patient-details',
@@ -11,20 +12,47 @@ import { Patient } from 'src/app/types/patient';
 })
 export class PatientDetailsComponent implements OnInit {
   isRowsChecked = false
-  id?: string
+  patientId?: string
   details?: Patient
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(async (params: ParamMap) => {
-      this.id = params.get('id') || ''
-      if (this.id) {
-        const response = await GraphqlService.client.request(GqlConstants.GET_PATIENT_DETAILS, { user: this.id })
-        this.details = response.user_by_pk
-        console.log(this.details)
+      this.patientId = params.get('id') || ''
+      if (this.patientId) {
+        console.log('patientId:', this.patientId);
+        // const response = await GraphqlService.client.request(GqlConstants.GET_PATIENT_DETAILS, { user: this.id })
+        // this.details = response.user_by_pk
+        // console.log(this.details)
       }
     })
+  }
+
+  async createNewSessionAndRedirect() {
+    const sessionId = await this.createNewSession()
+    this.goToLink(`${environment.activityEndpoint}?sessionId=${sessionId}`)
+  }
+
+  async createNewSession() {
+    const session = await GraphqlService.client.request(GqlConstants.CREATE_SESSION, { patient: this.patientId, careplan: '4e2aa726-b07f-4f44-a4fd-fc228c93bfc7' })
+    if (
+      session &&
+      session.insert_session &&
+      session.insert_session.returning &&
+      Array.isArray(session.insert_session.returning) &&
+      session.insert_session.returning.length == 1 &&
+      session.insert_session.returning[0].id
+    ) {
+      const sessionId = session.insert_session.returning[0].id
+      console.log('createSessionAndRedirect:sessionId', sessionId)
+      return sessionId
+    }
+  }
+
+  goToLink(url: string) {
+    console.log(`goToLink:Redirecting user to ${url}...`)
+    window.open(url, '_blank')
   }
 
   toogleRowsCheck() {
