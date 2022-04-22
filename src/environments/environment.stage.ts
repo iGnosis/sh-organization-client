75551948b2fc112437037d9b5899079a9ae4aabe
Deep Bhatt <dbhatt@pointmotion.us>
2022-04-22 15:02:36 +0530

    add staging env

---
+++ src/environments/environment.stage.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  cognitoUserPoolID: 'us-east-1_INWkmpaSb',
+  cognitoClientID: '4pc04vcpfodcgac8nk5svm911i',
+  gqlEndpoint: 'https://api.dev.pointmotioncontrol.com/v1/graphql',
+  servicesEndpoint: 'https://services.dev.pointmotioncontrol.com',
+  activityEndpoint: 'https://session.dev.pointmotioncontrol.com'
+};
