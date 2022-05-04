ecc299a593ba88244e91f2e58a89f03302b48086
Aman Gautam <aman@pointmotioncontrol.com>
2022-05-04 22:02:31 +0530

    feat: 🎸 allow auto bg color on avatar

--- src/app/widgets/avatar/avatar.component.ts
+++ src/app/widgets/avatar/avatar.component.ts
@@ -8,12 +8,23 @@ import { Component, Input, OnInit } from '@angular/core';
 export class AvatarComponent implements OnInit {
 
   @Input() text = '';
-  @Input() bgColor = '#000066'
+  @Input() bgColor: string | 'auto' = '#000066'
+  @Input() bgAuto = false;
   @Input() size: 'sm' | 'md' | 'lg'  = 'md'
 
-  constructor() { }
+  bgChoices = ['#000066', '#39B54A', '#FFB000', '#EB0000']
+
+  constructor() { 
+  }
 
   ngOnInit(): void {
+    if(this.bgAuto) {
+      let sum = 0
+      for (let i = 0; i < this.text.length; i++) {
+        sum += this.text.charCodeAt(i)
+      }
+      this.bgColor = this.bgChoices[sum % this.bgChoices.length]
+    }
   }
 
 }
