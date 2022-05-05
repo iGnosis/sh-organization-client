6919663a24ce0a3837a5969e35e844f0586ac298
Aman Gautam <aman@pointmotioncontrol.com>
2022-05-05 15:21:13 +0530

    fix: 🐛 Renamed Dashboard to Home for breadcrumbs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -29,7 +29,7 @@ const routes: Routes = [
     ]
   },
   {
-    path: 'app', component: PrivateComponent, canActivateChild: [PrivateGuard],data: { breadcrumb: "Dashboard" },
+    path: 'app', component: PrivateComponent, canActivateChild: [PrivateGuard], data: { breadcrumb: "Home" },
     children: [
       { path: 'dashboard', component: DashboardComponent },
       {
