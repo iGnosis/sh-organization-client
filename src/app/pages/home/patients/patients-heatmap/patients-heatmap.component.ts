5a582fed7c5cff54de8c7e0406481ef0efaad5e9
vigneshrajj <vigneshhrajj@gmail.com>
2022-09-27 16:41:19 +0530

    fix: heatmap shows data from previous month

--- src/app/pages/home/patients/patients-heatmap/patients-heatmap.component.ts
+++ src/app/pages/home/patients/patients-heatmap/patients-heatmap.component.ts
@@ -24,7 +24,7 @@ export class PatientsHeatmapComponent implements OnInit {
 
   async getHeatmapData(): Promise<void> {
     const date = new Date();
-    const firstDay = new Date(date.getFullYear(), date.getMonth() - 2, 1);
+    const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
     const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
     const response = await this.chartService.fetchPatientMonthlyCompletionData(
       firstDay.toISOString(),
