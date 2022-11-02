6a44846c572c5c88636717686dfda72ec5fbbf60
Deep Bhatt <dbhatt@pointmotion.us>
2022-11-02 12:26:49 +0530

    fix: 🐛 correct the prod env vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { Environment } from "src/app/pointmotion";
 export const environment: Environment = {
   name: 'prod',
   production: true,
-  gqlEndpoint: 'https://api.dev.pointmotioncontrol.com/v1/graphql',
-  servicesEndpoint: 'https://services.dev.pointmotioncontrol.com',
-  activityEndpoint: 'https://session.dev.pointmotioncontrol.com'
+  gqlEndpoint: 'https://api.prod.pointmotioncontrol.com/v1/graphql',
+  servicesEndpoint: 'https://services.prod.pointmotioncontrol.com',
+  activityEndpoint: 'https://session.prod.pointmotioncontrol.com'
 };
