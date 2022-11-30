0798fe59718e06419a714035dca770212c2c965e
Mohan Vaddi <myselfgeek01@gmail.com>
2022-11-30 11:25:29 +0530

    feat: 🎸 changed routing from /sessions to /game
    changed routing from '/sessions' to '/game' as we are having analytics
    for each game now instead of session. also added moving-tones in
    patient-details dropdown/
    
    ✅ Closes: SH-930; SH-851

--- src/app/pages/home/patients/patient-details/patient-details.component.ts
+++ src/app/pages/home/patients/patient-details/patient-details.component.ts
@@ -65,7 +65,13 @@ export class PatientDetailsComponent implements OnInit {
 
   engagementChartFilter?: string = undefined;
 
-  activityFilterOptions = ['sit_stand_achieve', 'beat_boxer', 'sound_explorer'];
+  availableGames = [
+    'sit_stand_achieve',
+    'beat_boxer',
+    'sound_explorer',
+    'moving_tones',
+  ];
+  activityFilterOptions = this.availableGames;
 
   toggleFilterDiv() {
     this.isShowFilter = !this.isShowFilter;
@@ -126,11 +132,14 @@ export class PatientDetailsComponent implements OnInit {
     navSpeed: 700,
     responsive: {
       940: {
-        items: 2
-      }
+        items: 2,
+      },
     },
     nav: true,
-    navText: [ '<i class="bi bi-chevron-left"></i>', '<i class="bi bi-chevron-right"></i>'],
+    navText: [
+      '<i class="bi bi-chevron-left"></i>',
+      '<i class="bi bi-chevron-right"></i>',
+    ],
   };
 
   constructor(
@@ -677,7 +686,7 @@ export class PatientDetailsComponent implements OnInit {
 
     let games: string[];
     if (!filter) {
-      games = ['sit_stand_achieve', 'beat_boxer', 'sound_explorer'];
+      games = this.availableGames;
     } else {
       games = filter;
     }
@@ -798,7 +807,7 @@ export class PatientDetailsComponent implements OnInit {
   // }
 
   openSessionDetailsPage(sessionId: string) {
-    this.router.navigate(['/app/sessions/', sessionId]);
+    this.router.navigate(['/app/game/', sessionId]);
   }
 
   engagementStartDate: Date;
