f07a1c49a216e373fa8d649a736b57788f2c69aa
Deep Bhatt <dbhatt@pointmotion.us>
2023-03-07 15:42:41 +0530

    fix: 🐛 show success ratio

--- src/app/pages/home/patients/patient-details/patient-details.component.ts
+++ src/app/pages/home/patients/patient-details/patient-details.component.ts
@@ -601,11 +601,9 @@ export class PatientDetailsComponent implements OnInit, OnDestroy {
             caretSize: 15,
             callbacks: {
               label: function (tooltipItem: any) {
-                // console.log('tooltipItem:', tooltipItem);
-                const data = tooltipItem.dataset.data[tooltipItem.dataIndex];
-                // const successRatio =
-                //   tooltipItem.dataset.data[tooltipItem.dataIndex];
-                return ` ${tooltipItem.dataset.label} - ${data}%`;
+                // console.log('tooltipItem.dataset:', tooltipItem.dataset);
+                const successRatio = tooltipItem.dataset.data[tooltipItem.dataIndex];
+                return ` ${tooltipItem.dataset.label} - ${successRatio}%`;
               },
             },
           },
